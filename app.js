49134b6a9cde30c40d476200996576be564cacd6
Chetana <chetanakaradiguddi@gmail.com>
2026-01-08 20:00:08 +0530

    added console log

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!");
+console.log("This is a simple Node.js application.");
+console.log("Enjoy coding!");
+console.log("Goodbye, World!");
+console.log("This is a simple Node.js application.");
